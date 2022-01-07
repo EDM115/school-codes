447521a1eaf648ccaf2c918234315416eabd0eae
EDM 1.15 <82015596+EDM115@users.noreply.github.com>
2022-01-07 15:19:27 +0100

    Delete Python/Boucle_Tantque directory

--- Python/Boucle_Tantque/defiA07.py
+++
@@ -1,8 +0,0 @@
-from gturtle import *
-joe=Turtle()
-    
-pg = joe.getPlayground()# pour utiliser poser une image dans l'aire de jeu
-pg.drawImage("chemin_rouge.png", 0, 0)# (0,0) coordonnées du centre de l'image dans l'aire de jeu  
-joe.penUp()# on lève le crayon pour que joe lise la couleur de l'image et non celle de son crayon
-joe.setPos(-175,50)
-    
